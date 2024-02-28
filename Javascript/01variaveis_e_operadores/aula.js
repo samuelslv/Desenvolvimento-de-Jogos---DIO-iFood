1d619bef967233d108ffa4a83c4988f9a5c1479e
Samuel Silva <samuelslv7@gmail.com>
2024-02-28 15:38:13 -0300

    modified:   Javascript/01variaveis_e_operadores/aula.js

--- Javascript/01variaveis_e_operadores/aula.js
+++ Javascript/01variaveis_e_operadores/aula.js
@@ -1,6 +1,7 @@
 
 let variavel = 10 / 10;
-const pi = 3.14;
+const PI = 3.14; // conveção de usar em MAIUSCULO
 
 console.log(variavel);
 console.log(pi);
+// https://developer.mozilla.org/pt-BR/docs/Learn/Getting_started_with_the_web
